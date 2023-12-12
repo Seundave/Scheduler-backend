ea9672311140796e539f5b6f27732ab6d5bfde4d
David <olayinkadavid62@yahoo.com>
2023-12-12 22:09:35 +0100

    create sign-in api route

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
+//Sign-up api route
 export const signup = async (req, res, next) => {
   const { email, password, department, governmentsector } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
@@ -15,6 +18,25 @@ export const signup = async (req, res, next) => {
     await newUser.save();
     res.status(201).json("User created successfully");
   } catch (error) {
-    next(error)
+    next(error);
+  }
+};
+
+//Sign-in api route
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const {password:pass, ...rest} = validUser._doc  //To remove the password from showing inside the data
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
