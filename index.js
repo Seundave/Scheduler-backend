4b797c121f2f5ded5a9d6f2adcacaf8f0dd026ec
David <olayinkadavid62@yahoo.com>
2023-12-12 05:03:30 +0100

    connect to database

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+
+const PORT = 5000 || process.env.PORT;
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Connected successfully")
+}).catch((error)=>{
+console.log(error)
+})
 
 const app = express()
+
+app.use(express.json());
+
+app.use(cookieParser());
 
 // app.get('',(req,res)=>{
 
 // })
 
 app.listen(3000, ()=>{
-    console.log("Server is running on port 3000")
+    console.log(`Server is running on port ${PORT}`)
 })
