e0ac89e17396607fd1b1483c0be54d8fadf44de7
David <olayinkadavid62@yahoo.com>
2023-12-12 05:45:06 +0100

    create a test api route

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import UserRouter from "./routes/user.route.js"
 dotenv.config();
 
-const PORT = 5000 || process.env.PORT;
+const PORT = 3000 || process.env.PORT;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -20,9 +21,7 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-// app.get('',(req,res)=>{
-
-// })
+app.use('/user', UserRouter)
 
 app.listen(3000, () => {
   console.log(`Server is running on port ${PORT}`);
